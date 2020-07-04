51ec3cfc28f3d49e1648ff781c3cc3cf87826e78
tashfeenrao <raotashfeen25@gmail.com>
2020-07-05 04:04:01 +0500

    added projects screen shots

--- js/typing2.js
+++ js/typing2.js
@@ -1,4 +1,4 @@
 const typed2 = new Typed('.typing2', {
-    strings: ["Lives in Lahore, Pakistan", "Open for Remote oppertunities"],
+    strings: ["Lives in Lahore, Pakistan", "Open for Remote opportunities"],
     typeSpeed: 70
   });
