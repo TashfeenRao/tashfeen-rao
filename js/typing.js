8c2963c97d06ef603b8e69ee1c3dc1c18e5a9d6f
tashfeenrao <raotashfeen25@gmail.com>
2020-07-05 03:11:36 +0500

    added about section

---
+++ js/typing.js
@@ -0,0 +1,4 @@
+const typed = new Typed('.typing', {
+    strings: ["Software Engineer", "Full Stack Developer"],
+    typeSpeed: 100
+  });
---
+++ js/typing2.js
@@ -0,0 +1,4 @@
+const typed2 = new Typed('.typing2', {
+    strings: ["Lives in Lahore, Pakistan", "Open for Remote oppertunities"],
+    typeSpeed: 70
+  });
