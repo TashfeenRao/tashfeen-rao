d250f59e380db293411fff00234082178731f82e
tashfeenrao <raotashfeen25@gmail.com>
2020-07-14 20:56:06 +0500

    updated about section removed grammer errors

--- js/typing.js
+++ js/typing.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.typing', {
-    strings: ["Software engineer", "Full-stack developer"],
+    strings: ["Software engineer", "Full-stack developer."],
     typeSpeed: 55,
     backSpeed: 55,
     loop: false,
